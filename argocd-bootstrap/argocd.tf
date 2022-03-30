64b61330beea26ceae7fe0d12b36ce44f656fed5
Leon Seng <w.leon.seng@gmail.com>
2022-03-30 17:09:12 +1100

    argocd-bootstrap: Ensure Argo CD UI still accessible until all apps have been removed

--- argocd-bootstrap/argocd.tf
+++ argocd-bootstrap/argocd.tf
@@ -23,76 +23,11 @@ resource "kubectl_manifest" "argocd" {
   override_namespace = var.argocd_namespace
 }
 
-# This ensures all Argo CD Application CRs are deleted before Argo CD installation is removed (along with the Application CRD).
-# Without this, the deletion of Application CRs will be stuck if the Application CRD is deleted before the CRs are cleaned up,
-# as the status of the CRs cannot be queried without the CRD
-resource "null_resource" "argocd_app_cleanup" {
-  depends_on = [kubectl_manifest.argocd]
-  triggers = {
-    kubeconfig_file  = var.kubeconfig_file
-    argocd_namespace = var.argocd_namespace
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<SCRIPT
-until [ $( \
-  kubectl get applications \
-    --kubeconfig ${self.triggers.kubeconfig_file} \
-    -n ${self.triggers.argocd_namespace} \
-    --no-headers \
-  2>/dev/null \
-  | wc -l \
-) -eq 0 ]; do
-sleep 10;
-done
-SCRIPT
-  }
-}
-
-############################################################################
-# Deploy Argo CD bootstrap Application that will install other Applications
-############################################################################
-# Optionally create a Secret for Argo CD to pull from private Git repository
-resource "kubernetes_secret" "private_repo_auth" {
-  depends_on = [null_resource.argocd_app_cleanup]
-  count      = var.bootstrap_app_source_repo.password == null ? 0 : 1
-  metadata {
-    name      = "private-repo-auth"
-    namespace = var.argocd_namespace
-    labels = {
-      "argocd.argoproj.io/secret-type" = "repository"
-    }
-  }
-
-  data = {
-    url      = var.bootstrap_app_source_repo.url
-    username = var.bootstrap_app_source_repo.username
-    password = var.bootstrap_app_source_repo.password
-  }
-}
-
-# need to use kubectl_manifest here because kubernetes_manifest does not support defining CRD and CR in the same TF state
-# https://github.com/hashicorp/terraform-provider-kubernetes/issues/1367
-resource "kubectl_manifest" "bootstrap_app" {
-  depends_on = [null_resource.argocd_app_cleanup]
-  wait       = true
-  yaml_body = templatefile(
-    abspath("${path.module}/bootstrap-app.yaml.tpl"),
-    {
-      argocd_namespace = var.argocd_namespace
-      url              = var.bootstrap_app_source_repo.url
-      revision         = var.bootstrap_app_source_repo.revision
-      path             = var.bootstrap_app_source_repo.path
-    }
-  )
-}
-
 ############################################################################
 # Expose argo CD web UI on public Load balancer
 ############################################################################
 resource "kubernetes_service" "argocd_server_lb" {
-  depends_on = [kubernetes_namespace.argocd]
+  depends_on = [kubectl_manifest.argocd]
 
   metadata {
     name      = "argocd-server-lb"
@@ -129,4 +64,69 @@ data "kubernetes_secret" "argocd_server_password" {
     name      = "argocd-initial-admin-secret"
     namespace = var.argocd_namespace
   }
+}
+
+############################################################################
+# Deploy Argo CD bootstrap Application that will install other Applications
+############################################################################
+# This ensures all Argo CD Application CRs are deleted before Argo CD installation is removed (along with the Application CRD).
+# Without this, the deletion of Application CRs will be stuck if the Application CRD is deleted before the CRs are cleaned up,
+# as the status of the CRs cannot be queried without the CRD
+resource "null_resource" "argocd_app_cleanup" {
+  depends_on = [kubernetes_service.argocd_server_lb]
+  triggers = {
+    kubeconfig_file  = var.kubeconfig_file
+    argocd_namespace = var.argocd_namespace
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<SCRIPT
+until [ $( \
+  kubectl get applications \
+    --kubeconfig ${self.triggers.kubeconfig_file} \
+    -n ${self.triggers.argocd_namespace} \
+    --no-headers \
+  2>/dev/null \
+  | wc -l \
+) -eq 0 ]; do
+sleep 10;
+done
+SCRIPT
+  }
+}
+
+# Optionally create a Secret for Argo CD to pull from private Git repository
+resource "kubernetes_secret" "private_repo_auth" {
+  depends_on = [null_resource.argocd_app_cleanup]
+  count      = var.bootstrap_app_source_repo.password == null ? 0 : 1
+  metadata {
+    name      = "private-repo-auth"
+    namespace = var.argocd_namespace
+    labels = {
+      "argocd.argoproj.io/secret-type" = "repository"
+    }
+  }
+
+  data = {
+    url      = var.bootstrap_app_source_repo.url
+    username = var.bootstrap_app_source_repo.username
+    password = var.bootstrap_app_source_repo.password
+  }
+}
+
+# need to use kubectl_manifest here because kubernetes_manifest does not support defining CRD and CR in the same TF state
+# https://github.com/hashicorp/terraform-provider-kubernetes/issues/1367
+resource "kubectl_manifest" "bootstrap_app" {
+  depends_on = [null_resource.argocd_app_cleanup]
+  wait       = true
+  yaml_body = templatefile(
+    abspath("${path.module}/bootstrap-app.yaml.tpl"),
+    {
+      argocd_namespace = var.argocd_namespace
+      url              = var.bootstrap_app_source_repo.url
+      revision         = var.bootstrap_app_source_repo.revision
+      path             = var.bootstrap_app_source_repo.path
+    }
+  )
 }
